be867a00ddca547beec8aea062a1cb6f240a85f4
timur-saitzyanov <71211623+timur-saitzyanov@users.noreply.github.com>
2023-01-29 22:28:44 +0300

    Added lotty animation

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css';
+import Lottie from 'react-lottie';
+import animationData from './lotties/typing-lotty.json';
+
+export default function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  return (
+    <div>
+      <Lottie
+        options={defaultOptions}
+        height={400}
+        width={400}
+      />
+    </div>
+  );
+}
